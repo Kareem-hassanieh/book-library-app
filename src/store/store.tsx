334fe3ca5623ee15e55c486e09539d2253f6b8ba
Kareem-hassanieh <kareemalh19@gmail.com>
2024-10-27 12:01:54 +0200

    updating the zustand store

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -10,10 +10,17 @@ interface Book {
 
 interface BookStore {
   recommendedBooks: Book[];
+  searchResults: Book[]; 
+  searchPerformed: boolean;
   setRecommendedBooks: (books: Book[]) => void;
+  setSearchResults: (books: Book[]) => void;
+  setSearchPerformed: (performed: boolean) => void; 
 }
-
 export const useStore = create<BookStore>((set) => ({
   recommendedBooks: [],
+  searchResults: [],
+  searchPerformed: false,
   setRecommendedBooks: (books) => set({ recommendedBooks: books }),
+  setSearchResults: (books) => set({ searchResults: books }),
+  setSearchPerformed: (performed) => set({ searchPerformed: performed }),
 }));
